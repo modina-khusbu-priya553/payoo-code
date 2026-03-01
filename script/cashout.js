bfa689e6f5e92b27acd92fec5f0f06648fd9e889
Modina Khusbu Priya <modinakhusbupriya@gmail.com>
2026-03-02 00:19:11 +0200

    design Home page

---
+++ script/cashout.js
@@ -0,0 +1,11 @@
+document.getElementById('cashout-btn').addEventListener('click', function(){
+
+    // 1 - get the agent number & validate it
+    // 2 - get the amount , validate it & convert it to number
+    // 3 - get the current Balance, validate it & convert it to number
+    // 4 - Calculate the new balance after cashout
+    // 5 - Get the pin and verify it
+    //  5.1 true:: show an alert > set the new balance > redirect to home page
+    // 5.2 false:: show an alert that the pin is wrong > return
+
+})
